bd21bc3fcc379d0799323f2a87671045f444254b
Guerric David <guerric.david@telecom-paris.fr>
2025-04-04 09:48:52 +0200

    to QT 6.9

--- Remote/mainwindow.cpp
+++ Remote/mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QLabel>
+#include <QHBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QHBoxLayout *mainLayout = new QHBoxLayout;
+    QLabel *label = new QLabel("Hello world");
+    mainLayout->addWidget(label);
+
+    QWidget *centralWidget = new QWidget(this);
+    centralWidget->setLayout(mainLayout);
+    this->setCentralWidget(centralWidget);
 }
 
 MainWindow::~MainWindow()
